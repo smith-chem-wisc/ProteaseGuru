2412e4d71eb548fb7aa5f6bdc4446b5ad44e6f82
RACHEL M MILLER <rmmiller22@wisc.edu>
2020-11-09 22:02:22 -0600

    remove unused code

--- Tasks/ProteaseGuruTask.cs
+++ Tasks/ProteaseGuruTask.cs
@@ -22,26 +22,6 @@ namespace Tasks
         }
         public abstract MyTaskResults RunSpecific(string OutputFolder, List<DbForDigestion> dbFileList);
         public abstract MyTaskResults RunSpecific(MyTaskResults digestionResults, List<string> peptideFilePaths);
-
-        public static event EventHandler<SingleTaskEventArgs> FinishedSingleTaskHandler;
-
-        public static event EventHandler<SingleFileEventArgs> FinishedWritingFileHandler;
-
-        public static event EventHandler<SingleTaskEventArgs> StartingSingleTaskHander;
-
-        public static event EventHandler<StringEventArgs> StartingDataFileHandler;
-
-        public static event EventHandler<StringEventArgs> FinishedDataFileHandler;
-
-        public static event EventHandler<StringEventArgs> OutLabelStatusHandler;
-
-        public static event EventHandler<StringEventArgs> WarnHandler;
-
-        public static event EventHandler<StringEventArgs> LogHandler;
-
-        public static event EventHandler<StringEventArgs> NewCollectionHandler;
-
-
         public MyTask TaskType { get; set; }
 
         protected static void Main()
