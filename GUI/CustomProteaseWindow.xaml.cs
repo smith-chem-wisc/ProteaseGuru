dc98325351cca9448403fd34a352ab1f985939a6
Michael Shortreed <mshort@chem.wisc.edu>
2023-11-01 12:19:16 -0500

    remove unused using statements

--- GUI/CustomProteaseWindow.xaml.cs
+++ GUI/CustomProteaseWindow.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using Proteomics;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Engine;
-using Tasks;
 using Proteomics.ProteolyticDigestion;
 using System.IO;
-using System.Globalization;
 using static Tasks.ProteaseGuruTask;
-using MzLibUtil;
-using System.ComponentModel;
 using Easy.Common.Extensions;
 
 namespace GUI
