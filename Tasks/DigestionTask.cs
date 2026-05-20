7e1884c88924227a6b60ce4fecb6ec256021b575
trishorts <mshort@chem.wisc.edu>
2026-05-20 13:42:29 -0500

    update to mzLib 579 (#72)

--- Tasks/DigestionTask.cs
+++ Tasks/DigestionTask.cs
@@ -380,7 +380,7 @@ namespace Tasks
                     var predictor = CheckoutPredictor();
                     try
                     {
-                        var result = predictor.PredictRetentionTime(peptides[i], out _);
+                        var result = predictor.PredictRetentionTimeEquivalent(peptides[i], out _);
                         results[i] = result ?? -1;
                     }
                     finally
